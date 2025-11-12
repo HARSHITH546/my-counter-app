c1b21822b24918d80b30fb046617a3175808b281
Harshith <harshithbharatha@gmail.com>
2025-11-12 12:37:21 +0530

    Initial commit-hello world

---
+++ my-counter-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <p>This is my first react app</p>
+    </div>
+  )
+}
+
+export default App
